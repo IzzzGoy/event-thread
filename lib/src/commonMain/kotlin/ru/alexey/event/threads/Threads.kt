fd397c70f9a5c65dc6e17608c5a22ea77c4dc159
IzzzGoy <xzadmoror@list.ru>
2023-10-17 17:16:43 +0300

    work

--- lib/src/commonMain/kotlin/ru/alexey/event/threads/Threads.kt
+++ lib/src/commonMain/kotlin/ru/alexey/event/threads/Threads.kt
@@ -1,73 +1,124 @@
 package ru.alexey.event.threads
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+import kotlin.reflect.KClass
+
 @OptIn(ExperimentalStdlibApi::class)
-interface EventThread<T>: AutoCloseable where T: Event, T: Any {
-    
-    var action: (Event) -> Unit
-    override fun close() {
-        TODO("Not yet implemented")
+abstract class  EventThread<T>: AutoCloseable where T: Event {
+
+    private val actionsMutable: MutableList<(Event) -> Unit> = mutableListOf()
+
+    val actions: List<(Event) -> Unit>
+        get() = actionsMutable
+
+    operator fun invoke(block: (Event) -> Unit) {
+        actionsMutable.add(block)
     }
 
-
 }
 
-interface Buildable {
-    fun build()
-}
+class ScopeEventsThreadBuilder {
+    val config: Config
+        by lazy {
+            ConfigBuilder().let { builder ->
+                configs.values.forEach {
+                    builder.apply(it)
+                }
 
-interface ScopeEventsThreadBuilder {
-    val eventBus: EventBus
-    val buildable: MutableSet<Buildable>
-}
+                object : Config() {
+                    override val eventBus: EventBus = builder.eventBus
+                    override fun <T : Any> get(clazz: KClass<T>): Datacontainer<T>? {
+                        return (builder.containers[clazz])?.let { it as Datacontainer<T> }
+                    }
+                }.also {
+                    builder.channel = true
+                }
+        }
+    }
 
-interface ThreadActionBuilder<TYPE, DC: Datacontainer<TYPE>> : ScopeEventsThreadBuilder {
-    val dcContext: () -> DC
-}
+    val configs = mutableMapOf<String, ConfigBuilder.() -> Unit>()
 
-fun eventsBuilder(eventBus: EventBus, block: ScopeEventsThreadBuilder.() -> Unit) {
-    object : ScopeEventsThreadBuilder {
-        override val eventBus: EventBus = eventBus
-        override val buildable: MutableSet<Buildable> = mutableSetOf()
-        init {
+    /*fun threads(block: Config.() -> Unit) {
+        with(config) {
             block()
-            buildable.forEach(Buildable::build)
         }
-    }
+    }*/
 }
 
+class ConfigBuilder {
+    var eventBus: EventBus = EventBus.defaultFactory()
+    val containers: MutableMap<KClass<out Any>, Datacontainer<out Any>>
+        = mutableMapOf()
 
+    var channel = false
+}
 
-inline fun<reified T: Event> ScopeEventsThreadBuilder.eventThread(noinline action: (T) -> Unit) : EventThread<T> {
+inline fun ScopeEventsThreadBuilder.config(noinline block: ConfigBuilder.() -> Unit) {
+    configs.put("config", block)
+}
 
-    return object : EventThread<T>, Buildable {
-        override var action: (Event) -> Unit = {
-            if (it is T) {
-                action(it)
+inline fun ScopeEventsThreadBuilder.containers(noinline block: ConfigBuilder.() -> Unit) {
+    configs.put("containers", block)
+}
+
+abstract class Config {
+    abstract val eventBus: EventBus
+    abstract operator fun<T: Any> get(clazz: KClass<T>): Datacontainer<T>?
+    inline fun <reified T: Any> resolve(): Datacontainer<T>? = get(T::class)
+    inline fun <reified T: Any> resolveOrThrow(): Datacontainer<T> = get(T::class) ?: throw Exception("${T::class.qualifiedName} not registered")
+
+    inline infix fun<reified T: Event, reified TYPE: Any> EventThread<T>.bind(noinline action: EventBus.(TYPE, T) -> TYPE) {
+        invoke { event ->
+            if (event is T) {
+                with(eventBus) {
+                    resolve<TYPE>()?.update { type: TYPE ->
+                        action(type, event)
+                    }
+                }
             }
         }
-        override fun close() {
-            eventBus.unsubscribe<T>()
-        }
-
-        override fun build() {
-            eventBus { this }
-        }
-    }.also {
-        this.buildable.add(it)
     }
-}
 
+    inline fun<reified T: Event> eventThread(noinline action: EventBus.(Event) -> Unit) : EventThread<T> {
 
-inline fun<reified T: Event, TYPE, DC: Datacontainer<TYPE>> EventThread<T> .bind(dc: DC, noinline action: TYPE.(T) -> TYPE) {
+        return object : EventThread<T>() {
+            override fun close() {
+                eventBus.unsubscribe<T>()
+            }
 
-     this.action = {
-        if (it is T) {
-            dc.update { type ->
-                type.action(it)
+            init {
+                val eventBus = eventBus
+                invoke {
+                    with(eventBus) {
+                        action(it)
+                    }
+                }
+                eventBus { this }
             }
         }
     }
 }
+
+
+
+fun eventsBuilder(block: ScopeEventsThreadBuilder.() -> Unit): Config {
+    val scope = ScopeEventsThreadBuilder().also(block)
+
+    return scope.config
+}
+
+
+
+
+
+
 
 
 
